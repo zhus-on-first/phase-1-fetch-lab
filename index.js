2ba6f58fc136dcabaa80c40c06a80b94296a8634
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-04 09:36:17 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
